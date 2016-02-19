7939fbf73e413e258442b390e709beacc579477d
Casey Link <unnamedrambler@gmail.com>
2016-02-19 14:36:07 +0100

    Fix build and tests after major ember upgrade

--- addon/services/keen-querying.js
+++ addon/services/keen-querying.js
@@ -1,33 +1,32 @@
+import Ember from 'ember';
 /* global Keen */
 
 // See keen-js SDK's query events docs for more info:
 // https://github.com/keen/keen-js/blob/master/docs/query.md#run-multiple-analyses-at-once
 
-import Ember from 'ember';
-
 export default Ember.Service.extend({
-  env: function() {
-    return this.container.lookupFactory('config:environment');
-  }.property(),
-  projectId: function() {   // String (required always)
+  env: Ember.computed(function () {
+    return Ember.getOwner(this).resolveRegistration('config:environment');
+  }),
+  projectId: Ember.computed('env', function () {   // String (required always)
     var projectId = this.get('env').KEEN_PROJECT_ID || Ember.$('meta[property="keen:project_id"]').attr('content') || window.KEEN_PROJECT_ID;
     if (!projectId) {
       Ember.Logger.info('Ember Keen Querying: Missing Keen project id, please set `ENV.KEEN_PROJECT_ID` in config.environment.js');
     }
     return projectId;
-  }.property('env'),
-  readKey: function() {     // String (required for sending data)
+  }),
+  readKey: Ember.computed('env', function () {   // String (required for sending data)
     var readKey = this.get('env').KEEN_READ_KEY || Ember.$('meta[property="keen:read_key"]').attr('content') || window.KEEN_READ_KEY;
     if (!readKey) {
       Ember.Logger.info('Ember Keen Querying: Missing Keen read key, please set `ENV.KEEN_READ_KEY` in config.environment.js');
     }
     return readKey;
-  }.property('env'),
+  }),
   protocol: "auto",         // String (optional: https | http | auto)
   host: "api.keen.io/3.0",  // String (optional)
   requestType: null,        // String (optional: jsonp, xhr, beacon)
 
-  client: function() {
+  client: Ember.computed('projectId', 'readKey', 'protocol', 'host', 'requestType', function () {
     return new Keen({
       projectId: this.get('projectId'),
       readKey: this.get('readKey'),
@@ -35,13 +34,13 @@ export default Ember.Service.extend({
       host: this.get('host'),
       requestType: this.get('requestType')
     });
-  }.property('projectId', 'readKey', 'protocol', 'host', 'requestType'),
+  }),
 
-  query: function(analysisType, eventOrParams) {
+  query: function (analysisType, eventOrParams) {
     var self = this;
     var analysis = this._prepareAnalysis(analysisType, eventOrParams);
-    return new Ember.RSVP.Promise(function(resolve, reject) {
-      self.get('client').run(analysis, function(err, res){
+    return new Ember.RSVP.Promise(function (resolve, reject) {
+      self.get('client').run(analysis, function (err, res) {
         if (err) {
           reject(err);
         }
@@ -54,15 +53,15 @@ export default Ember.Service.extend({
 
   // argument structure = {{analysisType1: eventOrParams1}, {analysisType2: eventOrParams2}, ...}
   // https://github.com/keen/keen-js/blob/master/docs/query.md#run-multiple-analyses-at-once
-  multiQuery: function(queriesData) {
+  multiQuery: function (queriesData) {
     var self = this;
     var analyses = [];
     for (var key in queriesData) {
       var newAnalysis = this._prepareAnalysis(key, queriesData[key]);
       analyses.push(newAnalysis);
     }
-    return new Ember.RSVP.Promise(function(resolve,reject) {
-      self.get('client').run(analyses, function(err, res) {
+    return new Ember.RSVP.Promise(function (resolve, reject) {
+      self.get('client').run(analyses, function (err, res) {
         if (err) {
           reject(err);
         }
@@ -73,10 +72,10 @@ export default Ember.Service.extend({
     });
   },
 
-  _prepareAnalysis: function(analysisType, eventOrParams) {
+  _prepareAnalysis: function (analysisType, eventOrParams) {
     var params = eventOrParams;
     if (Ember.typeOf(eventOrParams) === 'string') {
-      params = { eventCollection: eventOrParams };
+      params = {eventCollection: eventOrParams};
     }
     return new Keen.Query(analysisType, params);
   }
--- tests/unit/services/keen-querying-test.js
+++ tests/unit/services/keen-querying-test.js
@@ -1,102 +1,107 @@
 import Ember from 'ember';
 import { moduleFor, test } from 'ember-qunit';
-import env from 'dummy/config/environment';
-import QUnit from 'qunit';
 import keenClientStub from '../../helpers/stubs';
 import { KeenQueryMock } from '../../helpers/stubs';
+import env from 'dummy/config/environment';
 
-moduleFor('service:keen-querying', 'KeenQueryingService', {
+moduleFor('service:keen-querying', 'Unit | Service | keen-querying', {
   setup: function() {
     env.KEEN_PROJECT_ID = "project321";
     env.KEEN_READ_KEY = "read321";
-    this.container.register('config:environment', env);
+    Ember.getOwner(this).register('config:environment', env);
   }
 });
 
-test('project id uses ENV variable', function() {
-  expect(1);
-  var service = this.subject();
-  equal(service.get('projectId'), "project321");
+// Replace this with your real tests.
+test('it exists', function(assert) {
+  let service = this.subject();
+  assert.ok(service);
 });
 
-test('project id defaults to global if no env variable or meta tag exists', function(){
-  expect(1);
+test('project id uses ENV variable', function(assert) {
+  assert.expect(1);
+  var service = this.subject();
+  assert.equal(service.get('projectId'), "project321");
+});
+
+test('project id defaults to global if no env variable or meta tag exists', function(assert){
+  assert.expect(1);
   env.KEEN_PROJECT_ID = null;
   window.KEEN_PROJECT_ID = "123project";
   var service = this.subject();
-  equal(service.get('projectId'), "123project");
+  assert.equal(service.get('projectId'), "123project");
 });
 
-test('read key uses ENV variable', function() {
-  expect(1);
+test('read key uses ENV variable', function(assert) {
+  assert.expect(1);
   var service = this.subject();
-  equal(service.get('readKey'), "read321");
+  assert.equal(service.get('readKey'), "read321");
 });
 
-test('read key defaults to global if no env variable or meta tag exists', function(){
-  expect(1);
+test('read key defaults to global if no env variable or meta tag exists', function(assert){
+  assert.expect(1);
   env.KEEN_READ_KEY = null;
   window.KEEN_READ_KEY = "123read";
   var service = this.subject();
-  equal(service.get('readKey'), "123read");
+  assert.equal(service.get('readKey'), "123read");
 });
 
 /**
  * @todo change ENV variables and meta tag at runtime, and test them
  */
 
-test('query returns a promise that resolves if success', function() {
+test('query returns a promise that resolves if success', function(assert) {
   var service = this.subject({
     client: keenClientStub.create({successMsg: "Yay"})
   });
   var promise = service.query('analysisType', 'event');
   promise.then(function(response) {
-    equal(response, "Yay");
+    assert.equal(response, "Yay");
   });
 });
 
-test('query returns a promise to catch if there\'s an error', function() {
+test('query returns a promise to catch if there\'s an error', function(assert) {
   var service = this.subject({
     client: keenClientStub.create({failing: true, failureMsg: "Womp"})
   });
   var promise = service.query('analysisType', 'event');
   promise.catch(function(reason) {
-    equal(reason, "Womp");
+    assert.equal(reason, "Womp");
   });
 });
 
-test('multiQuery returns a promise that resolves if success', function() {
+test('multiQuery returns a promise that resolves if success', function(assert) {
   var service = this.subject({
     client: keenClientStub.create({successMsg: "Hoorah"})
   });
   var promise = service.query('analysisType', 'event');
   promise.then(function(response) {
-    equal(response, "Hoorah");
+    assert.equal(response, "Hoorah");
   });
 });
 
-test('multiQuery returns a promise to catch if there\'s an error', function() {
+test('multiQuery returns a promise to catch if there\'s an error', function(assert) {
   var service = this.subject({
     client: keenClientStub.create({failing: true, failureMsg: "Boo"})
   });
   var promise = service.query('analysisType', 'event');
   promise.catch(function(reason) {
-    equal(reason, "Boo");
+    assert.equal(reason, "Boo");
   });
 });
 
-test('_prepareAnalysis returns a new Keen.Query', function() {
+test('_prepareAnalysis returns a new Keen.Query', function(assert) {
   window.Keen.Query = KeenQueryMock;
   var service = this.subject();
   var keenQuery = service._prepareAnalysis("count", {});
-  equal(keenQuery.constructor.name, "KeenQueryMock");
+  assert.equal(keenQuery.constructor.name, "KeenQueryMock");
 });
 
-test('_prepareAnalysis accepts either an object or a string for its second argument', function() {
+test('_prepareAnalysis accepts either an object or a string for its second argument', function(assert) {
   window.Keen.Query = KeenQueryMock;
   var service = this.subject();
   var keenQuery1 = service._prepareAnalysis("sum", {eventCollection: "wiggles"});
   var keenQuery2 = service._prepareAnalysis("sum", "wiggles");
-  equal(keenQuery1.params.eventCollection, "wiggles", "params accepts object as is");
-  equal(keenQuery2.params.eventCollection, "wiggles", "params adds string as eventCollection property");
+  assert.equal(keenQuery1.params.eventCollection, "wiggles", "params accepts object as is");
+  assert.equal(keenQuery2.params.eventCollection, "wiggles", "params adds string as eventCollection property");
 });
